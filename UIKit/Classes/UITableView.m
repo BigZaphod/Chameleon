0850fa1ca9e171ee18bed656272100d976e051f5
BigZaphod <sean@iconfactory.com>
2010-09-08 19:18:22 +0000

    Another tweak to the logic that keeps cells in the view as often as possible as long as they don't mess up the actual visible cells.
    git-svn-id: svn://dev.iconfactory.net/Projects/Software/UIKit/trunk@3857 d3437ac4-8b1b-0410-be28-b75ab53cb89f

--- UIKit/Classes/UITableView.m
+++ UIKit/Classes/UITableView.m
@@ -350,24 +350,26 @@ const CGFloat _UITableViewDefaultRowHeight = 43;
 	for (UITableViewCell *cell in [availableCells allValues]) {
 		if (cell.reuseIdentifier) {
 			[_reusableCells addObject:cell];
-			
-			// only remove a reusable cell if it's within the visible bounds, otherwise don't worry about it and leave it in the tableview.
-			// this is done becaue when resizing a table view by shrinking it's height in an animation, it looks better. The reason is that
-			// when an animation happens, it sets the frame to the new (shorter) size and thus recalcuates which cells should be visible.
-			// If it removed all non-visible cells, then the cells on the bottom of the table view would disappear immediately but before
-			// the frame of the table view has actually animated down to the new, shorter size. So the animation is jumpy/ugly because
-			// the cells suddenly disappear instead of seemingly animating down and out of view like they should.
-			if (CGRectIntersectsRect(cell.frame,visibleBounds)) {
-				[cell removeFromSuperview];
-			}
 		} else {
-			// always remove non-cached cells - otherwise we lose track of them.
 			[cell removeFromSuperview];
 		}
 	}
 	
 	// non-reusable cells should end up dealloced after at this point, but reusable ones live on in _reusableCells.
 	[availableCells release];
+	
+	// now make sure that all available (but unused) reusable cells aren't on screen in the visible area.
+	// this is done becaue when resizing a table view by shrinking it's height in an animation, it looks better. The reason is that
+	// when an animation happens, it sets the frame to the new (shorter) size and thus recalcuates which cells should be visible.
+	// If it removed all non-visible cells, then the cells on the bottom of the table view would disappear immediately but before
+	// the frame of the table view has actually animated down to the new, shorter size. So the animation is jumpy/ugly because
+	// the cells suddenly disappear instead of seemingly animating down and out of view like they should. This tries to leave them
+	// on screen as often as possible, but only if they don't get in the way.
+	for (UITableViewCell *cell in _reusableCells) {
+		if (CGRectIntersectsRect(cell.frame,visibleBounds)) {
+			[cell removeFromSuperview];
+		}
+	}
 	
 	if (_tableFooterView) {
 		CGRect tableFooterFrame = _tableFooterView.frame;
