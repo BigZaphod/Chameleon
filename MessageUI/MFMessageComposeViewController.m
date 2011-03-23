944731b4ac899c5acb5df1a0972aefa2d3df791a
Peter Steinberger <me@petersteinberger.com>
2011-03-24 00:45:58 +0100

    add Message Composer View for MessageUI (stub)

---
+++ MessageUI/MFMessageComposeViewController.m
@@ -0,0 +1,16 @@
+//
+//  MFMessageComposeViewController.m
+//  MessageUI
+//
+//  Created by Peter Steinberger on 24.03.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MFMessageComposeViewController.h"
+
+
+@implementation MFMessageComposeViewController
+
+@synthesize messageComposeDelegate = _messageComposeDelegate;
+
+@end
