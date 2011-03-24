418bd7423ebc332a74ff7d1194361efa5730eb13
Sean Heber <sean@iconfactory.com>
2011-03-23 19:10:25 -0500

    Whoops. Accidently removed UIScreen.h. Let's pretend that didn't happen, shall we? :P

--- UIKit/Classes/UIViewController.m
+++ UIKit/Classes/UIViewController.m
@@ -35,6 +35,7 @@
 #import "UINavigationController.h"
 #import "UISplitViewController.h"
 #import "UIToolbar.h"
+#import "UIScreen.h"
 
 @implementation UIViewController
 @synthesize view=_view, wantsFullScreenLayout=_wantsFullScreenLayout, title=_title, contentSizeForViewInPopover=_contentSizeForViewInPopover;
