f07a5661c9c19cd69d284f083ba21b29381e5958
Jason Allum <jason@dmgctrl.com>
2011-07-13 19:48:17 -0400

    Reworked UITableViewCell's selection machinery.
    This seems to be a much better approximation of iOS behavior, as verified in the simulator.

---
+++ UIKit/Classes/UITableViewCellUnhighlightedState.h
@@ -0,0 +1,7 @@
+@class UIColor;
+
+@interface UITableViewCellUnhighlightedState : NSObject
+@property (nonatomic, retain) UIColor *backgroundColor;
+@property (nonatomic, assign) BOOL highlighted;
+@property (nonatomic, assign) BOOL opaque;
+@end
