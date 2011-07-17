4ea56426936990666be950db8a03fac860a4d9cd
Jason Allum <jason@dmgctrl.com>
2011-07-17 13:20:01 -0400

    Removed unused declarations.

--- UIKit/Classes/UIProxyObject.h
+++ UIKit/Classes/UIProxyObject.h
@@ -4,12 +4,6 @@
 
 @property (retain) NSString* proxiedObjectIdentifier;
 
-+ (void) removeMappingsForCoder:(NSCoder*)coder;
-+ (id) mappedObjectForCoder:(NSCoder*)coder withIdentifier:(NSString*)identifier;
-+ (void) addMappings:(NSDictionary*)mappings forCoder:(NSCoder*)coder;
-+ (void) addMappingFromIdentifier:(NSString*)identifier toObject:(NSString*)object forCoder:(NSCoder*)coder;
-+ (CFDictionaryRef) proxyDecodingMap;
-
 - (id) initWithCoder:(NSCoder*)coder;
 - (void)encodeWithCoder:(NSCoder*)coder;
 
