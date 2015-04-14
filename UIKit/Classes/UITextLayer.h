018bbca395b12839c9fe61020718f65fa5e5fb42
Rob Nielsen <rob.nielsen@toushay.com>
2015-04-14 16:54:00 -0400

    Change containerView from id to UIView*

--- UIKit/Classes/UITextLayer.h
+++ UIKit/Classes/UITextLayer.h
@@ -67,7 +67,7 @@
 @end
 
 @interface UITextLayer : CALayer {
-    id containerView;
+    UIView *containerView;
     BOOL containerCanScroll;
     UICustomNSTextView *textView;
     UICustomNSClipView *clipView;
